1c0681c7ce993039ae35586dac358fc5899f7f04
Android-Artisan <romartisan2025@gmail.com>
2026-07-13 11:42:25 +0200

    unica: patches: bt-library-patch: adapt for b0s
    - ghirda be hard asf
    
    Signed-off-by: Android-Artisan <romartisan2025@gmail.com>

--- unica/patches/bt-lib-patch/customize.sh
+++ unica/patches/bt-lib-patch/customize.sh
@@ -31,4 +31,4 @@ fi
 # Before: [tbnz w8, #0, #0xbd260]
 # After: [b #0xbd260]
 HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
-    "2897773948050037" "289777392a000014"
+    "2897673948050037" "289767392a000014"

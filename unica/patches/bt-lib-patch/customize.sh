522db7b8606291bb4b126f07f1cf25f549b04d2e
Android-Artisan <romartisan2025@gmail.com>
2026-07-13 16:57:16 +0200

    unica: patches: BT-lib: fixup
    Signed-off-by: Android-Artisan <romartisan2025@gmail.com>

--- unica/patches/bt-lib-patch/customize.sh
+++ unica/patches/bt-lib-patch/customize.sh
@@ -30,10 +30,7 @@ fi
 # Disable VaultKeeper support
 # Before: [tbnz w8, #0, #0xbd260]
 # After: [b #0xbd260]
-if [[ "$TARGET_CODENAME" == "r8s" ]]; then
-    HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
-        "2897773948050037" "289777392a000014"
-else
-    HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
-        "2897673948050037" "289767392a000014"
-fi
+HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
+    "2897773948050037" "289777392a000014" || \
+HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
+    "2897673948050037" "289767392a000014"
